dc9de9ef3c03738df4ecbffc497a439c77cec1c9
vinicius matheus de souza <viniciusmsms@gmail.com>
2023-11-29 11:44:25 -0300

    Adicionado os métodos salvar e exibir todos em UnidadeMedidaController

--- ProjetoFinal/src/main/java/controller/produto/UnidadeMedidaController.java
+++ ProjetoFinal/src/main/java/controller/produto/UnidadeMedidaController.java
@@ -3,27 +3,67 @@ package controller.produto;
 import controller.Controller;
 import model.produto.UnidadeMedida;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import Util.DBConnection;
 
 public class UnidadeMedidaController implements Controller<UnidadeMedida> {
+	
+    private DBConnection dbConnection;
 
-    @Override
-    public boolean salvar(UnidadeMedida unidadeMedida) {
-        return false;
+	public UnidadeMedidaController(DBConnection dbConnection) {
+        this.dbConnection = dbConnection;
     }
 
-    @Override
-    public List<UnidadeMedida> exibirTodos() {
-        return null;
-    }
+	@Override
+	public boolean salvar(UnidadeMedida unidadeMedida) {
 
-    @Override
-    public UnidadeMedida exibir() {
-        return null;
-    }
+		try (Connection connection = dbConnection.getConnection();
+				PreparedStatement preparedStatement = connection
+						.prepareStatement("INSERT INTO UNIDADEMEDIDA (descricao,sigla) VALUES (?,?)")) {
+			preparedStatement.setString(1, unidadeMedida.getDescricao());
+			preparedStatement.setString(2, unidadeMedida.getSigla());
+			return true;
+		} catch (SQLException e) {
+			throw new RuntimeException("Erro ao salvar Unidade de medida", e);
+		}
+	}
 
-    @Override
-    public void excluir() {
+	@Override
+	public List<UnidadeMedida> exibirTodos() {
+		try (Connection connection = dbConnection.getConnection();
+				PreparedStatement preparedStatement = connection
+						.prepareStatement("SELECT * FROM UNIDADEMEDIDA");
+				ResultSet resultSet = preparedStatement.executeQuery()) {
+			List<UnidadeMedida> medidas = new ArrayList<>();
+			Map<Long, UnidadeMedida> medidaMap = new HashMap<>();
+			while (resultSet.next()) {
+				UnidadeMedida medida = new UnidadeMedida();
+				medida.setId(resultSet.getLong("id"));
+				medida.setDescricao(resultSet.getString("descricao"));
+				medida.setSigla(resultSet.getString("sigla"));
+				medidas.add(medida);
+			}
+			return medidas;
+		} catch (SQLException e) {
+			throw new RuntimeException("Erro ao exibir todas as medidas", e);
+		}
+	}
 
-    }
+	@Override
+	public UnidadeMedida exibir() {
+		return null;
+	}
+
+	@Override
+	public void excluir() {
+
+	}
 }
