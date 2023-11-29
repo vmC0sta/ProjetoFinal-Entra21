e1ead64ab0a8d54b6dbb685296343190f0e7ee84
FilipeWillian22 <filipewilliannonato@gmail.com>
2023-11-29 13:00:33 -0300

    Categoria Produto adicionada

--- ProjetoFinal/src/main/java/controller/produto/CategoriaController.java
+++ ProjetoFinal/src/main/java/controller/produto/CategoriaController.java
@@ -2,27 +2,68 @@ package controller.produto;
 
 import controller.Controller;
 import model.produto.Categoria;
+import Util.DBConnection;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CategoriaController implements Controller<Categoria> {
+    private DBConnection dbConnection;
+
+    public CategoriaController(DBConnection dbConnection) {
+        this.dbConnection = dbConnection;
+    }
+
     @Override
     public boolean salvar(Categoria categoria) {
-        return false;
+        try (
+                Connection connection = dbConnection.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(
+                        "INSERT INTO categoria (descricao) VALUES (?)")
+        ) {
+            preparedStatement.setString(1, categoria.getDescricao());
+
+            preparedStatement.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao salvar categoria", e);
+        }
     }
 
     @Override
     public List<Categoria> exibirTodos() {
-        return null;
+        try (
+                Connection connection = dbConnection.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(
+                        "SELECT * FROM categoria");
+                ResultSet resultSet = preparedStatement.executeQuery()
+        ) {
+            List<Categoria> categorias = new ArrayList<>();
+            while (resultSet.next()) {
+                Categoria categoria = new Categoria();
+                categoria.setId(resultSet.getLong("id"));
+                categoria.setDescricao(resultSet.getString("descricao"));
+
+                categorias.add(categoria);
+            }
+            return categorias;
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao exibir todas as categorias", e);
+        }
     }
 
     @Override
     public Categoria exibir(Long id) {
+       
         return null;
     }
 
     @Override
     public void excluir(Long id) {
-
+        
     }
 }
